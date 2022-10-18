b30baf49ee3577581903c9232b49ca11691e66e6
maina-glitch <maina.kelvin0805@gmail.com>
2022-10-18 14:27:00 -0700

    New changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
+ *main -Entry point
  *
- *
- * Always returns (0) Success
+ *return - Always returns (0) Success
  *
  */
-
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
